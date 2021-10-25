edae39e025bdea9a92e9715f793f83f81207a3d3
kockono <kockonoop@gmail.com>
2021-10-24 18:59:52 -0600

    js-angular

---
+++ pwa-portafolio/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PortafolioComponent } from './portafolio/portafolio.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PortafolioComponent
+  ],
+  imports: [
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
