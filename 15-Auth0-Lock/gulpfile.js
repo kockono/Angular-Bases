45750305324b8bbf26cd64068c4717b0a33141f0
kockono <kockonoop@gmail.com>
2022-10-09 03:33:44 -0600

    Auth0 Js

---
+++ 15-Auth0-Lock/gulpfile.js
@@ -0,0 +1,23 @@
+var gulp = require('gulp'),
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglify'),
+    rename = require('gulp-rename'),
+    ngAnnotate = require('gulp-ng-annotate'),
+    injectVersion = require('gulp-inject-version'),
+    sourceFiles = [
+      'angular-lock.js'
+    ];
+
+gulp.task('default', function() {
+  return gulp.src(sourceFiles)
+    .pipe(injectVersion({
+      prepend: '',
+      replace: /%%PACKAGE_VERSION%%/g
+    }))
+    .pipe(concat('angular-lock.js'))
+    .pipe(ngAnnotate())
+    .pipe(gulp.dest('./dist/'))
+    .pipe(uglify())
+    .pipe(rename('angular-lock.min.js'))
+    .pipe(gulp.dest('./dist'))
+});
